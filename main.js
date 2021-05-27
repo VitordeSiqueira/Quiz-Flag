cac0405fd4a73e3f5bc1eed4d88d17418e23853a
vitor dutra <vitor-siqueira2019@hotmail.com>
2021-05-27 12:37:52 -0300

    Refatoração 2.0

--- main.js
+++ main.js
@@ -1,93 +1,91 @@
 
-let perguntas = 
-[
-    {
-        id: 1,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Indonesia', 'B) Japão', 'C) Chile', 'D) Polônia'],
-        resposta: 'A',
+let perguntas =
+    [
+        {
+            id: 1,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Indonesia', 'B) Japão', 'C) Chile', 'D) Polônia'],
+            resposta: 'A',
 
-    },
+        },
 
-    {
-        id: 2,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Rússia', 'B) Argentina', 'C) Mexico', 'D) Holanda'],
-        resposta: 'C'
-    },
+        {
+            id: 2,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Rússia', 'B) Argentina', 'C) Mexico', 'D) Holanda'],
+            resposta: 'C'
+        },
 
-    {
-        id: 3,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Venezuela', 'B) Egito', 'C) Líbano', 'D) Marrocos'],
-        resposta: 'C'
-    },
+        {
+            id: 3,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Venezuela', 'B) Egito', 'C) Líbano', 'D) Marrocos'],
+            resposta: 'C'
+        },
 
-    {
-        id: 4,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Egito', 'B) Honduras', 'C) Argentina', 'D) Coreia do Sul'],
-        resposta: 'B'
-    },
+        {
+            id: 4,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Egito', 'B) Honduras', 'C) Argentina', 'D) Coreia do Sul'],
+            resposta: 'B'
+        },
 
-    {
-        id: 5,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Noruega', 'B) Itália', 'C) Irlanda', 'D) Costa do Marfim'],
-        resposta: 'D'
-    },
+        {
+            id: 5,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Noruega', 'B) Itália', 'C) Irlanda', 'D) Costa do Marfim'],
+            resposta: 'D'
+        },
 
-    {
-        id: 6,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Rússia', 'B) França', 'C) Eslováquia', 'D) Nigeria'],
-        resposta: 'A'
-    },
+        {
+            id: 6,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Rússia', 'B) França', 'C) Eslováquia', 'D) Nigeria'],
+            resposta: 'A'
+        },
 
-    {
-        id: 7,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Bulgaria', 'B) Hungria', 'C) Brasil', 'D) China'],
-        resposta: 'A'
-    },
+        {
+            id: 7,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Bulgaria', 'B) Hungria', 'C) Brasil', 'D) China'],
+            resposta: 'A'
+        },
 
-    {
-        id: 8,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Guiné', 'B) Áustria', 'C) Holanda', 'D) Peru'],
-        resposta: 'D'
-    },
+        {
+            id: 8,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Guiné', 'B) Áustria', 'C) Holanda', 'D) Peru'],
+            resposta: 'D'
+        },
 
-    {
-        id: 9,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Espanha', 'B) França', 'C) Portugal', 'D) Egito'],
-        resposta: 'A'
-    },
+        {
+            id: 9,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Espanha', 'B) França', 'C) Portugal', 'D) Egito'],
+            resposta: 'A'
+        },
 
-    {
-        id: 10,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Uruguai', 'B) Marrocos', 'C) Egito', 'D) China'],
-        resposta: 'B'
-    },
+        {
+            id: 10,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Uruguai', 'B) Marrocos', 'C) Egito', 'D) China'],
+            resposta: 'B'
+        },
 
-    {
-        id: 11,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) Guatemala', 'B) Argentina', 'C) Honduras', 'D) Japão'],
-        resposta: 'A'
-    },
+        {
+            id: 11,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) Guatemala', 'B) Argentina', 'C) Honduras', 'D) Japão'],
+            resposta: 'A'
+        },
 
-    {
-        id: 12,
-        pergunta: "Qual o pais dessa bandeira? ",
-        alternatives: ['A) África do Sul', 'B) Brasil', 'C) Holanda', 'D) Coreia do Sul'],
-        resposta: 'D'
-    },
-];
-
-
+        {
+            id: 12,
+            pergunta: "Qual o pais dessa bandeira? ",
+            alternatives: ['A) África do Sul', 'B) Brasil', 'C) Holanda', 'D) Coreia do Sul'],
+            resposta: 'D'
+        },
+    ];
 
 const bandeiras = [
     "./Bandeiras/Bandeira-Indonesia-1.svg",
@@ -104,29 +102,30 @@ const bandeiras = [
     "./Bandeiras/Bandeira-Coreia-12.svg"
 ]
 
-function initial() {
-    let numberQuestion = document.querySelector('.question > h2')
-    let numberQuestionScore = document.querySelector('.score > h2')
-    let flag = document.querySelector('#myImg')
-    let labels = document.querySelectorAll('.label')
-    let arr = [...labels]
-    let check = document.getElementsByName('opt')
-    let arrCheck = [...check]
-    
+let span = document.querySelectorAll(`span`)
+let arraySpan = [...span]
+let check = document.getElementsByName('opt')
+let arrCheck = [...check]
+let flag = document.querySelector('#myImg')
+let numberQuestion = document.querySelector('.question > h2')
+let numberQuestionScore = document.querySelector('.score > h2')
+let labels = document.querySelectorAll('label')
+let arr = [...labels]
 
-    for(let i = 0; i <= arr.length-1; i++) {
+function initial() {
+
+    for (let i = 0; i <= arr.length - 1; i++) {
         arr[i].innerHTML = perguntas[0].alternatives[i]
     }
-
 
     numberQuestion.innerHTML = `Bandeira ${1}`
     numberQuestionScore.innerHTML = `Questão ${1}/12`
 
     flag.setAttribute('src', bandeiras[indice])
-    
+
     for (const i in arrCheck) {
-        
-        if(arrCheck[i].checked) {
+
+        if (arrCheck[i].checked) {
             arrCheck[i].checked = false
         }
     }
@@ -138,136 +137,104 @@ let score = 0
 
 function nextQuestion() {
 
-    let check = document.getElementsByName('opt')
-    let arrCheck = [...check]
-
-    
-
-    
-    for(let i = 0; i <= arrCheck.length-1;i++) {
-        if(arrCheck[i].checked) {
-           if(arrCheck[i].value === perguntas[indice].resposta) {
-            score++
-           }
+    for (let i = 0; i <= arrCheck.length - 1; i++) {
+        if (arrCheck[i].checked) {
+            if (arrCheck[i].value === perguntas[indice].resposta) {
+                score++
+            }
         }
     }
 
-    console.log(score)
-    
     for (const i in arrCheck) {
-        
-        if(arrCheck[i].checked) {
+
+        if (arrCheck[i].checked) {
             arrCheck[i].checked = false
         }
     }
 
-
-    changeFlag() 
+    changeFlag()
     changeNumberQuestion()
     changeFlag()
     alternativesLabel()
     scoreScreen()
-    
-    
-
-    let labels = document.querySelectorAll('label')
-    let arr = [...labels]
 
     for (const i of arr) {
-        i.removeAttribute("id")  
+        i.removeAttribute("id")
 
     }
 
-    if(indice >= 12) {
+    if (indice >= 12) {
         endQuestions()
     }
-    
+
 }
 
 let idQuestion = 1
 
-
 function alternativesLabel() {
-    let labels = document.querySelectorAll('.label')
-    let arr = [...labels]
-    
-    if(idQuestion >= 12) {
+
+    if (idQuestion >= 12) {
         return endQuestions()
     }
-    
-    for(let i = 0; i <= arr.length-1; i++) {
-        arr[i].innerText = perguntas[idQuestion].alternatives[i] 
+
+    for (let i = 0; i <= arr.length - 1; i++) {
+        arr[i].innerText = perguntas[idQuestion].alternatives[i]
     }
 
     idQuestion++
 
 }
-     document.body.addEventListener('change', function(e) {
-         let target = e.target
-         let radio = document.getElementsByName('opt')
-         let arrayRadio = [...radio]
-        
 
-         if(target.value === 'A' || target.value === 'B' || target.value === 'C' || target.value === 'D'){
-             let labels = document.querySelectorAll('.label')
-             let arra = [...labels]
-    
-             for (const i of arra) {
-                i.removeAttribute('id')
+document.body.addEventListener('change', function (e) {
+    let target = e.target
+    let radio = document.getElementsByName('opt')
 
-                if(i.className[0] === target.value) {
-                    i.setAttribute('id', 'labels')
-                }       
+    if (target.value === 'A' || target.value === 'B' || target.value === 'C' || target.value === 'D') {
+        let labels = document.querySelectorAll('.label')
+        let arra = [...labels]
 
-             }
+        for (const i of arra) {
+            i.removeAttribute('id')
 
-         }
-     })
-    
-     
+            if (i.className[0] === target.value) {
+                i.setAttribute('id', 'labels')
+            }
 
-     
+        }
 
+    }
+})
 
 function changeNumberQuestion() {
     indice++
 
-    if(indice > 11){
+    if (indice > 11) {
         indice = 11
     }
 
     let id = perguntas[indice].id
-    let numberQuestion = document.querySelector('.question > h2')
-    let numberQuestionScore = document.querySelector('.score > h2')
 
     numberQuestion.innerHTML = `Bandeira ${id}`
     numberQuestionScore.innerHTML = `Questão ${id}/12`
-
 }
 
 
 function changeFlag() {
-    let flag = document.querySelector('#myImg')
     flag.setAttribute('src', bandeiras[indice])
 }
 
 function endQuestions() {
     let myModal = document.querySelector('.myModal')
-    let pPorcent = document.querySelector('.porcent')
     let p = document.querySelector('.modal-content > p')
 
     myModal.style.display = "block"
     p.innerHTML = `Parabéns você acertou ${score} questões`
 
-} 
-function scoreScreen() {
-    let span = document.querySelectorAll(`span`)
-    let arraySpan = [...span]
-
-    arraySpan[indice].classList.add('active')
 }
 
-
+function scoreScreen() {
+    arraySpan[indice].classList.add('active')
+}
 
 function newGame() {
     indice = 0
@@ -279,14 +246,8 @@ function newGame() {
     let myModal = document.querySelector('.myModal')
     myModal.style.display = "none"
 
-    let span = document.querySelectorAll(`span`)
-    let arraySpan = [...span]
-
-    for(let i = 0; i <= arraySpan.length; i++) {
+    for (let i = 0; i <= arraySpan.length; i++) {
         arraySpan[i].classList.remove('active')
     }
-    
+
 }
-
-   
-
